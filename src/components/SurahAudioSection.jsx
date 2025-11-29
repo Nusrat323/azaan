4c7917b8350c3bdb3f7cdba0893a2204806c51cb
Nusrat323 <samanusrat18@gmail.com>
2025-11-29 19:07:52 +0600

    add button

--- src/components/SurahAudioSection.jsx
+++ src/components/SurahAudioSection.jsx
@@ -236,7 +236,7 @@ export default function SurahAudioSection() {
           <button
             onClick={prevPage}
             disabled={currentPage === 1}
-            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 disabled:opacity-50"
+            className="px-4 py-2 bg-indigo-100 rounded hover:bg-indigo-300 disabled:opacity-50"
           >
             Prev
           </button>
@@ -246,7 +246,7 @@ export default function SurahAudioSection() {
           <button
             onClick={nextPage}
             disabled={currentPage === totalPages}
-            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 disabled:opacity-50"
+            className="px-4 py-2 bg-indigo-100 rounded hover:bg-indigo-300 disabled:opacity-50"
           >
             Next
           </button>
