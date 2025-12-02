1478768289aacc9dded9487958fe0fdb2efeafb7
Nusrat323 <samanusrat18@gmail.com>
2025-12-02 20:04:11 +0600

    add

--- src/components/AzaanPlayer.jsx
+++ src/components/AzaanPlayer.jsx
@@ -1,13 +1,11 @@
 
 import React from "react";
-import fajrSound from "../assets/fajr.mp4";
-import otherSound from "../assets/other.mp3";
 
 export default function AzaanPlayer({ fajrAudio, otherAudio }) {
   return (
     <>
-      <audio ref={fajrAudio} src={fajrSound} preload="auto" />
-      <audio ref={otherAudio} src={otherSound} preload="auto" />
+      <audio ref={fajrAudio} src="/azaan/fajr.mp3" preload="auto" />
+      <audio ref={otherAudio} src="/azaan/other.mp3" preload="auto" />
     </>
   );
 }
